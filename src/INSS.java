77388b71677a78a109416f3631f793c8713badce
CTT <gustavosilva90@gmail.com>
2022-11-21 13:45:17 -0300

    PRIMEIRAS ATIVIDADES DE CONDICIONAIS

--- src/INSS.java
+++ src/INSS.java
@@ -1,7 +1,34 @@
 
+import java.util.Scanner;
 public class INSS {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        double salario,faixaUm = 1212, desFaixaUm=0.075;
+        double  faixaDois = 2427.35,descFaixaDois=0.09, calcFaixaDois;
+        double faixaTres=3641.03,desFaixaTres=0.12, calcFaixaTres;
+        double desFaixaQuatro = 0.14,calFaixaQuatro,faixaQuatro = 7087.22,
+                descontoEmprego,tetoMax = (faixaQuatro - faixaTres)*desFaixaQuatro;;
+        double calUm = faixaUm * desFaixaUm, calDois = (faixaDois - faixaUm) * descFaixaDois,
+                calTres= (faixaTres - faixaDois)*desFaixaTres;
+        System.out.print("Digite o sálario R$ ");
+        salario = scan.nextDouble();
 
+        if (salario <=faixaUm){
+            descontoEmprego = desFaixaUm * salario;
+        }else if (salario <= faixaDois){
+            calcFaixaDois = (salario - faixaUm)*descFaixaDois;
+            descontoEmprego = calUm + calcFaixaDois;
+        }else if (salario <= faixaTres){
+            calcFaixaTres = (salario - faixaDois)*desFaixaTres;
+            descontoEmprego = calUm + calDois + calcFaixaTres;
+        }else if (salario <=faixaQuatro){
+            calFaixaQuatro = (salario - faixaTres)*desFaixaQuatro;
+            descontoEmprego = calUm + calDois + calTres + calFaixaQuatro;
+        }else {
+            descontoEmprego = calUm + calDois + calTres + tetoMax;
+        }
+        System.out.printf("O salário bruto informado foi %.2f reais.%n" +
+                "O valor de contribuição ao INSS é %.2f reais",salario,descontoEmprego);
     }
 }
--- src/Sorteio.java
+++ src/Sorteio.java
@@ -1,7 +1,28 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Sorteio {
     public static void main(String[] args) {
+        Random random = new Random(6);
         Scanner scanner = new Scanner(System.in);
+
+        int numero;
+        int aleatorio = random.nextInt(6);
+
+        System.out.println("Digite um numero de 1 a 6");
+        numero = scanner.nextInt();
+
+        if (numero <= 1 && numero >= 6) {
+            System.out.println("O numero selecionado foi " + numero);
+            System.out.println("Erro! Por favor, escolha um numero entre 1 e 6.");
+        } else if (numero == aleatorio) {
+            System.out.println("O numero selecionado foi " + numero);
+            System.out.println("O numero sorteado foi " + aleatorio);
+            System.out.println("Você acertou");
+        } else if (numero != aleatorio) {
+            System.out.println("O numero selecionado foi " + numero);
+            System.out.println("O numero sorteado foi " + aleatorio);
+            System.out.println("Voce Errou");
+        }
     }
 }
